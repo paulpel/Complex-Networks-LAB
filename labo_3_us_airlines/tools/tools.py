9f9675064f41d75b701efd4be4197e74cc9b3d1c
Paul <pawelpelar@gmail.com>
2021-10-14 19:34:27 +0200

    Shapes fix

--- labo_3_us_airlines/tools/tools.py
+++ labo_3_us_airlines/tools/tools.py
@@ -19,6 +19,7 @@ class UsAirlines:
         self.create_output_dir()
         self.export_json()
         self.print_graph()
+        print(len(self.airport_data['connections']))
     
     def load_data(self):
         file_path = os.path.join(self.script_path, 'data', 'USAir97.net')
@@ -41,6 +42,7 @@ class UsAirlines:
                     vert = list_name[0]
                     name = ' '.join(list_name[1:-3])
                     x_y_z = [float(elem) for elem in line.split()[-3:]]
+                    x_y_z[1] = 1 - x_y_z[1]
 
                     self.airport_data['airports'][vert] = {
                         'name': name[1:-1],
@@ -69,12 +71,31 @@ class UsAirlines:
         for airport in self.airport_data['airports']:
             labels[int(airport)] = self.airport_data['airports'][airport]['name']
             pos[int(airport)] = self.airport_data['airports'][airport]['x_y_z_cords'][:-1]
+
         return labels, pos
 
+    def color_specific_nodes(self, G):
+        color_map = []
+        highlight_nodes = [150, 23]
+        for node in G:
+            if node in highlight_nodes:
+                color_map.append('#CEFF00')
+            else:
+                color_map.append('#FF8243')
+        return color_map
+
     def print_graph(self):
+        fig, ax = plt.subplots(figsize=(15,8))
         G = nx.Graph()
         G.add_weighted_edges_from(self.airport_data['connections'])
+
         labels, pos = self.prepare_for_networkx()
-        nx.draw(G, pos=pos, labels=labels, with_labels=True, font_size=8,
-            edge_color='#fc8eac', node_size=100)
+        color_map = self.color_specific_nodes(G)
+
+        nx.draw(G, pos=pos, labels=labels, with_labels=True, font_size=6,
+            edge_color='#C8A2C8', node_size=200, node_color=color_map,
+            node_shape='h')
+
+        fig.set_facecolor('#6D9BC3')
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
         plt.show()
